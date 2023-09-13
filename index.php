db4744dd9a9d67d412318b29d4c918240ccc7480
cristian-18cg <104658855+cristian-18cg@users.noreply.github.com>
2023-09-12 20:33:08 -0500

    15 ejercicios php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  include_once './procesar.php';
 
  $objOperaciones =new Operaciones(12,4);
- $objPromedio =new Promedio(10,10,8,10,10,10,30,10,10,10);
+ $objPromedio =new Promedio(30,0,2,5,3,15,28,11,15,10);
  
 
  echo ("<h1>Operaciones</h1>");
@@ -18,11 +18,37 @@
 
  echo ("<h3>Division</h3>");
  echo ("<p>".$objOperaciones->getdatos()." / ".$objOperaciones->getdatos2(). " = ".$objOperaciones-> getDivisionFirst()."</p>");
+
  echo ("<h3>Potencia</h3>");
  echo ("<p>".$objOperaciones->getdatos()." ^ ".$objOperaciones->getdatos2(). " = ".$objOperaciones->getPotenciaFirst()."</p>");
  
+ echo ("<h3>Raíz Cuadrada</h3>");
+ echo ("<p>Raiz de: ".$objOperaciones->getdatos()." = ".$objOperaciones->getraiz()."</p>");
+
+ echo ("<h3>Modulo</h3>");
+ echo ("<p>".$objOperaciones->getdatos()." mod ".$objOperaciones->getdatos2(). " = ".$objOperaciones->getModulo()."</p>");
+
+ echo ("<h3>Porcentaje </h3>");
+ echo ("<p>El ".$objOperaciones->getdatos2()." porciento de ".$objOperaciones->getdatos(). " = ".$objOperaciones->getporcentaje()."</p>");
+
+ echo ("<h3>Area de un circulo </h3>");
+ echo ("<p>El Area de un circulo con radio ".$objOperaciones->getdatos(). " = ".$objOperaciones->getAreaCirculo()."</p>");
+
  echo ("<h3>Promedio</h3>");
- echo ("<p>".$objPromedio->getprome()."</p>");
+ echo ("<p>El promedio de 30+0+2+5+3+15+28+11+15+10 es: ".$objPromedio->getprome()."</p>");
+
+ echo ("<h3>Conversión de Celsius a Fahrenheit</h3>");
+ echo ("<p>".$objOperaciones->getdatos()." grados Celsius son equivalentes a " .$objOperaciones->getCelcuisaFarenheit()." grados Fahrenheit</p>");
+
+ echo ("<h3>Suma de los Primeros N Números Naturales</h3>");
+ echo ("<p>La suma de los primeros ".$objOperaciones->getdatos()." números naturales es: " .$objOperaciones->getnumnat()."</p>");
+
+ echo ("<h3>Resolución de Ecuación Cuadrática</h3>");
+ echo($objPromedio->ecuacioncua());
+
+ echo ("<h3>Área de un Triángulo</h3>");
+ echo ("<p>Base: ".$objOperaciones->getdatos()." Altura: ".$objOperaciones->getdatos2(). " Area = ".$objOperaciones->getareatriangulo()."</p>");
 
 
-?>
+ echo ("<h3>Perímetro de un Cuadrado</h3>");
+ echo ("<p>El perímetro de un cuadrado con lado ".$objOperaciones->getdatos()." es ".$objOperaciones->getarearectangulo()."</p>");
--- procesar.php
+++ procesar.php
@@ -2,52 +2,102 @@
 class Operaciones
 {
 
- public $valor1;
- public $valor2;
- public $result;
+    public $valor1;
+    public $valor2;
+    public $result;
 
-public function __construct($valor1, $valor2)
+    public function __construct($valor1, $valor2)
+    {
+        $this->valor1 = $valor1;
+        $this->valor2 = $valor2;
+    }
+
+    public function getdatos()
+    {
+        return $this->valor1;
+    }
+    public function getdatos2()
+    {
+        return $this->valor2;
+    }
+    public function getSumaFirst()
+    {
+        $result = $this->valor1 + $this->valor2;
+        return $result;
+    }
+
+    public function getMultiplicacionFirst()
+    {
+        $result = $this->valor1 * $this->valor2;
+        return $result;
+    }
+    public function getDivisionFirst()
+    {
+        $result = $this->valor1 / $this->valor2;
+        return $result;
+    }
+    public function getPotenciaFirst()
+    {
+        $result = $this->valor1 ** $this->valor2;
+        return $result;
+    }
+    public function getRestaFirst()
+    {
+        $result = $this->valor1 - $this->valor2;
+        return $result;
+    }
+    public function getporcentaje()
+    {
+        $result = ($this->valor1 * $this->valor2) / 100;
+        return $result;
+    }
+    public function getModulo()
+    {
+        $result = $this->valor1 % $this->valor2;
+        return $result;
+    }
+    public function getAreaCirculo()
+    {
+        $result = pi() * pow($this->valor1, 2);
+        return $result;
+    }
+    public function getCelcuisaFarenheit()
+    {
+        $result = ($this->valor1 * 9 / 5) + 32;
+        return $result;
+    }
+    public function getareatriangulo()
+    {
+        $result = ($this->valor1 * $this->valor2) / 2;
+        return $result;
+    }
+
+    public function getarearectangulo()
+    {
+        $result = $this->valor1 *  4;
+        return $result;
+    }
+    public function getnumnat()
+    {
+        $N = $this->valor1; // Cambia el valor de N según tus necesidades
+
+        $suma = 0;
+        for ($i = 1; $i <= $N; $i++) {
+            $suma += $i;
+        }
+        $result = $suma;
+        return $result;
+    }
+    public function getraiz()
+    {
+        $result = sqrt($this->valor1);
+        return $result;
+    }
+}
+
+
+class Promedio
 {
-    $this->valor1=$valor1;
-    $this->valor2=$valor2;
-}
-
-public function getdatos(){
-    return $this->valor1;
-}
-public function getdatos2(){
-    return $this->valor2;
-}
-public function getSumaFirst(){
-    $result=$this->valor1 + $this->valor2;
-    return $result;
-}
-
-public function getMultiplicacionFirst(){
-    $result=$this->valor1 * $this->valor2;
-    return $result;
-}
-public function getDivisionFirst(){
-    $result=$this->valor1 / $this->valor2;
-    return $result;
-}
-public function getPotenciaFirst(){
-    $result=$this->valor1 ** $this->valor2;
-    return $result;
-}
-public function getRestaFirst(){
-    $result=$this->valor1 - $this->valor2;
-    return $result;
-}
-public function getporcentaje(){
-    $result=$this->valor1 - $this->valor2;
-    return $result;
-}
-
-}
-
-
-class Promedio{
     public $valor1;
     public $valor2;
     public $valor3;
@@ -59,27 +109,39 @@ class Promedio{
     public $valor9;
     public $valor10;
     public $result2;
-    
-   public function __construct($valor1, $valor2,$valor3,$valor4,$valor5,$valor6,$valor7,$valor8,$valor9,$valor10)
-   {
-       $this->valor1=$valor1;
-       $this->valor2=$valor2;
-       $this->valor1=$valor3;
-       $this->valor2=$valor4;
-       $this->valor1=$valor5;
-       $this->valor2=$valor6;
-       $this->valor1=$valor7;
-       $this->valor2=$valor8;
-       $this->valor1=$valor9;
-       $this->valor2=$valor10;
-   } 
+    public $discriminante;
+    public function __construct($valor1, $valor2, $valor3, $valor4, $valor5, $valor6, $valor7, $valor8, $valor9, $valor10)
+    {
+        $this->valor1 = $valor1;
+        $this->valor2 = $valor2;
+        $this->valor3 = $valor3;
+        $this->valor4 = $valor4;
+        $this->valor5 = $valor5;
+        $this->valor6 = $valor6;
+        $this->valor7 = $valor7;
+        $this->valor8 = $valor8;
+        $this->valor9 = $valor9;
+        $this->valor10 = $valor10;
+    }
 
-   public function getprome(){
-    $result2=$this->valor1 + $this->valor2 +$this->valor3 + $this->valor4+$this->valor5 + $this->valor6 + $this->valor7 + $this->valor8 + $this->valor9 +$this->valor10;
-    $result2=$result2/10;
-    return $result2;
+    public function getprome()
+    {
+        $result2 = $this->valor1 + $this->valor2 + $this->valor3 + $this->valor4 + $this->valor5 + $this->valor6 + $this->valor7 + $this->valor8 + $this->valor9 + $this->valor10;
+        $result2 = $result2 / 10;
+        return $result2;
+    }
+    public function ecuacioncua()
+    {
+        $discriminante = pow($this->valor3, 2) - 4 * $this->valor1 * $this->valor3;
+        if ($discriminante > 0) {
+            $raiz1 = (-$this->valor3 + sqrt($discriminante)) / (2 * $this->valor1);
+            $raiz2 = (-$this->valor3 - sqrt($discriminante)) / (2 * $this->valor1);
+            return ("<p>Las raíces son x₁ = $raiz1 y x₂ = $raiz2</p>");
+        } elseif ($discriminante == 0) {
+            $raiz = -$this->valor3 / (2 * $this->valor1);
+            return ("<p>La raíz doble es x = $raiz</p>");
+        } else {
+            return ("<p>La ecuación no tiene raíces reales.</p>");
+        }
+    }
 }
-
-
-}
-?>
